d302e9074a40ef3594a986bff939073661bbdba7
3102005 <cristhianpadron2005@gmail.com>
2024-11-15 20:51:26 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hola mundo, Cristhian Alfonzo Angyalbert Padron Alvarez C.I: 31031669 Seccion: 4, si');
+  res.send('Hola mundo, Cristhian Alfonzo Angyalbert Padron Alvarez C.I: 31031669 Seccion: 4');
 });
 
 module.exports = router;

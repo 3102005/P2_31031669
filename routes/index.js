acb1a2215c8af5a886407fd8355a649fed0b4a29
3102005 <cristhianpadron2005@gmail.com>
2025-04-09 18:34:00 -0400

    EstefaniG-17
    estefa

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('hola mundo, Estefani Jeannielys Gonzalez Gonzalez C.I: 32.057.736 seccion: 4');
-});
-
-module.exports = router;
